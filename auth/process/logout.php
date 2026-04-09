73523e5c0c7854b340a9bd361e96825be4a24e84
Bimaaaaaa <bmarzky.official@gmail.com>
2026-04-09 18:38:36 +0700

    fix: session validation in dashboard/index.php.

--- auth/process/logout.php
+++ auth/process/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-session_destroy();
-
-header("Location: ../login.php");
-exit();
+session_unset();    // Kosongkan variabel session
+session_destroy();  // Hancurkan session di server
+header("Location: ../login.php"); // Balik ke halaman login
+exit(); 
