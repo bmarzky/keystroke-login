b728bbd00e2d96687a0e4f3dfb7a4f6105ae7743
Bimaaaaaa <bmarzky.official@gmail.com>
2026-04-10 21:06:04 +0700

    style: remove trailing whitespaces in guide.js

--- assets/tutorial/guide.js
+++ assets/tutorial/guide.js
@@ -1,7 +1,7 @@
 const runTutorial = () => {
     // 1. Cek storage
     const hasSeenTutorial = localStorage.getItem('keystroke_tutorial_done');
-    
+
     // Log untuk memastikan fungsi terpanggil
     console.log("Memeriksa tutorial... Status di storage:", hasSeenTutorial);
 
@@ -11,7 +11,7 @@ const runTutorial = () => {
 
     // 2. Inisialisasi Driver dengan cara yang lebih aman untuk versi IIFE
     const driver = window.driver.js.driver;
-    
+
     const driverObj = driver({
         showProgress: true,
         allowClose: false,
@@ -26,35 +26,35 @@ const runTutorial = () => {
             console.log("Tutorial selesai, storage diupdate.");
         },
         steps: [
-            { 
-                popover: { 
-                    title: 'Selamat Datang!', 
-                    description: 'Sistem Login ini menggunakan <b>Keystroke Dynamics</b> untuk mengenali Kamu melalui cara Kamu mengetik.' 
-                } 
+            {
+                popover: {
+                    title: 'Selamat Datang!',
+                    description: 'Sistem Login ini menggunakan <b>Keystroke Dynamics</b> untuk mengenali Kamu melalui cara Kamu mengetik.'
+                }
             },
-            { 
-                element: '#username', 
-                popover: { 
-                    title: 'Kolom Username', 
+            {
+                element: '#username',
+                popover: {
+                    title: 'Kolom Username',
                     description: 'Masukkan username Kamu. <br><br><b>Tips:</b> Tekan tombol <b>Enter</b> untuk pindah ke kolom password secara instan.',
-                    side: "bottom", 
-                    align: 'start' 
-                } 
+                    side: "bottom",
+                    align: 'start'
+                }
             },
-            { 
-                element: '#password', 
-                popover: { 
-                    title: 'Kolom Password', 
+            {
+                element: '#password',
+                popover: {
+                    title: 'Kolom Password',
                     description: 'Ketik kata sandi Kamu seperti biasa. <br><br>Sistem akan mengenali <b>"Gaya Unik Mengetikmu"</b>, yaitu seberapa lama Kamu menekan tombol dan seberapa cepat jemarimu berpindah antar tombol. <br><br><b>Tips:</b> Ketik password Kamu dalam satu aliran. Sebisa mungkin hindari menghapus (<b>Backspace</b>) agar pola ketikanmu terekam sempurna.',
-                    side: "bottom", 
-                    align: 'start' 
-                } 
+                    side: "bottom",
+                    align: 'start'
+                }
             },
-            { 
-                popover: { 
-                    title: 'Penting!', 
-                    description: 'Gunakan kebiasaan mengetik yang sama saat <b>Register</b> dan <b>Login</b> demi akurasi verifikasi.' 
-                } 
+            {
+                popover: {
+                    title: 'Penting!',
+                    description: 'Gunakan kebiasaan mengetik yang sama saat <b>Register</b> dan <b>Login</b> demi akurasi verifikasi.'
+                }
             },
         ]
     });
