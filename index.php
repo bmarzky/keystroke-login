1ebc7e2ba6225507ee944b7b2611398743d2f2d1
Bimaaaaaa <bmarzky.official@gmail.com>
2026-04-17 20:17:19 +0700

    fix: add access logging for debugging in index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 // File rute utama
 session_start();
 
+// Log akses untuk debugging
+error_log("Access to index.php at " . date('Y-m-d H:i:s'));
+
 if (isset($_SESSION['user_id'])) {
     header("Location: dashboard/index.php");
 } else {
